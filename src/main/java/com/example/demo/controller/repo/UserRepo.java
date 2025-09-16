515688b6c49ac2d5a0e6f8691047c746639ce978
SandulRenuja <155486294+SandulRenuja@users.noreply.github.com>
2025-09-17 00:29:13 +0530

    backend

---
+++ src/main/java/com/example/demo/controller/repo/UserRepo.java
@@ -0,0 +1,9 @@
+package com.example.demo.controller.repo;
+
+import com.example.demo.controller.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer> {
+}
